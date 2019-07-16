3161e26e40906d3342432d2aefe87b0e523759f7
Philip Claren <philip.claren@googlemail.com>
2019-07-16 11:48:27 +0200

    added logger which can be overridden with custom logger function

--- src/settings.ts
+++ src/settings.ts
@@ -7,6 +7,7 @@ const POSTFIX_BASE64 = '-Base64';
 const semaphors: Array<Promise<void>> = [];
 let config: any;
 let hasDecryptionFinished = false;
+let logger = console.log;
 
 function decryptObject(decrypt: CryptFunction, obj: any): void {
   for (const k in obj) {
@@ -14,15 +15,19 @@ function decryptObject(decrypt: CryptFunction, obj: any): void {
       decryptObject(decrypt, obj[k]);
     } else {
       if (k.endsWith(POSTFIX_ENCRYPTED)) {
+        logger(`akec: "${k}" needs to be decrypted`);
         const promise = decrypt(obj[k]);
         semaphors.push(promise);
         promise.then((val: string) => {
           obj[k.substring(0, k.length - POSTFIX_ENCRYPTED.length)] = val;
           delete obj[k];
+          logger(`akec: "${k}" decryption finished`);
         });
       } else if (k.endsWith(POSTFIX_BASE64)) {
+        logger(`akec: "${k}" needs to be decoded`);
         obj[k.substring(0, k.length - POSTFIX_BASE64.length)] = Buffer.from(obj[k], 'base64').toString();
         delete obj[k];
+        logger(`akec: "${k}" decoding finished`);
       }
     }
   }
@@ -34,18 +39,20 @@ interface IKeyVaultAccessConfig {
   keyIdentifier: string;
 }
 
-export const init = (configFilePath: string, keyVaultAccessConfig: IKeyVaultAccessConfig) => {
-  const { clientId, clientSecret, keyIdentifier } = keyVaultAccessConfig;
-  const keyVault = new KeyVault(clientId, clientSecret, keyIdentifier);
+type Logger = (msg: any[]) => void;
 
+export const init = (configFilePath: string, keyVaultAccessConfig: IKeyVaultAccessConfig, customLogger?: Logger) => {
   const data = fs.readFileSync(configFilePath);
   config = JSON.parse(data.toString());
 
-  decryptObject(keyVault.decrypt, config);
+  initWithConfigContent(config, keyVaultAccessConfig, logger)
 };
 
-export const initWithConfigContent = (configContent: any, keyVaultAccessConfig: IKeyVaultAccessConfig) => {
+export const initWithConfigContent = (configContent: any, keyVaultAccessConfig: IKeyVaultAccessConfig, customLogger?: Logger) => {
   config = configContent;
+  if (customLogger) {
+    logger = customLogger;
+  }
 
   const { clientId, clientSecret, keyIdentifier } = keyVaultAccessConfig;
   const keyVault = new KeyVault(clientId, clientSecret, keyIdentifier);
