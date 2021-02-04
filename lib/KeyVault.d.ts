bb2794d707696620f9e8a944a9fdac63a691a2e6
Philip Claren <philip@claren.io>
2021-02-04 15:35:25 +0100

    build

--- lib/KeyVault.d.ts
+++ lib/KeyVault.d.ts
@@ -1,22 +1,17 @@
-import { EncryptionAlgorithm } from '@azure/keyvault-keys';
 import { Logger } from './types';
 export declare class KeyVault {
     private static localEncrypt;
     private static localDecrypt;
     private readonly keysClient;
-    private readonly vaultBaseUri;
     private readonly keyName;
     private readonly keyVersion;
-    private readonly algorithm;
-    private readonly tenant;
-    private readonly clientId;
-    private readonly clientSecret;
     private readonly credentials;
+    private readonly algorithm;
     private key;
     private cryptographyClientInstance;
     private logger?;
     private storedLogMessages;
-    constructor(tenant: string, clientId: string, clientSecret: string, keyIdentifier: string, algorithm?: EncryptionAlgorithm);
+    constructor(...args: any[]);
     setLogger(logger: Logger): void;
     encrypt: (payload: string) => Promise<string>;
     decrypt: (payload: string) => Promise<string>;
